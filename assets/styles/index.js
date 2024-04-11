0f370c065eeb66c0d840c0a20e4467a55eb61434
seanbsu <122116208+seanbsu@users.noreply.github.com>
2024-04-11 16:54:37 -0600

    add unmatch button when elipses pressed in messages

--- assets/styles/index.js
+++ assets/styles/index.js
@@ -268,6 +268,22 @@ export default StyleSheet.create({
 		justifyContent: "space-between",
 		alignItems: "center"
 	},
+	modalBackground: {
+		flex: 1,
+		backgroundColor: "rgba(0, 0, 0, 0.5)",
+		justifyContent: "center",
+		alignItems: "center",
+	  },
+	  modalContent: {
+		backgroundColor: "white",
+		padding: 20,
+		borderRadius: 10,
+		elevation: 5,
+	  },
+	  dropdownItem: {
+		fontSize: 16,
+		paddingVertical: 10,
+	  },
 
 	// CONTAINER - PROFILE
 	containerProfile: { marginHorizontal: 0 },
