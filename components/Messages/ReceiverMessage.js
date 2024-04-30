4c6edc97a0b14149e50c7af1c45fca246da9ea58
seanbsu <seancalkins@u.boisestate.edu>
2024-04-30 10:35:34 -0600

    base64 support for reciever message uri

--- components/Messages/ReceiverMessage.js
+++ components/Messages/ReceiverMessage.js
@@ -4,44 +4,57 @@ import { View, Text, Image } from "react-native";
 const ReceiverMessage = ({ message, receiver, timestamp }) => {
   return (
     <View style={{ alignSelf: "flex-start", marginHorizontal: 10, marginVertical: 5 }}>
-    <View
-      style={{
-        backgroundColor: "#FF6347",
-        borderRadius: 10,
-        borderBottomLeftRadius: 0,
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        marginHorizontal: 20,
-        marginVertical: 5,
-        marginLeft: 64,
-        alignSelf: "flex-start",
-        
-      }}
-    >
-      <Image
+      <View
         style={{
-          width: 48,
-          height: 48,
-          borderRadius: 24,
-          position: "absolute",
-          top: 0,
-          left: -50,
-        }}
-        source={receiver.uri}
-      />
-      <Text
-        style={{
-          color: "white"
+          backgroundColor: "#FF6347",
+          borderRadius: 10,
+          borderBottomLeftRadius: 0,
+          paddingHorizontal: 15,
+          paddingVertical: 10,
+          marginHorizontal: 20,
+          marginVertical: 5,
+          marginLeft: 64,
+          alignSelf: "flex-start",
         }}
       >
-        {message}
-      </Text>
-      
-    </View>
+        {typeof receiver.uri === 'number' ? (
+          <Image
+            style={{
+              width: 48,
+              height: 48,
+              borderRadius: 24,
+              position: "absolute",
+              top: 0,
+              left: -50,
+            }}
+            source={receiver.uri}
+          />
+        ) : (
+          <Image
+            style={{
+              width: 48,
+              height: 48,
+              borderRadius: 24,
+              position: "absolute",
+              top: 0,
+              left: -50,
+            }}
+            source={{ uri: receiver.uri }}
+          />
+        )}
+        <Text
+          style={{
+            color: "white"
+          }}
+        >
+          {message}
+        </Text>
+      </View>
       <Text style={{ alignSelf: "flex-end", marginTop: 5, fontSize: 12, color: "gray" }}>{formatTimestamp(timestamp)}</Text>
     </View>
   );
 };
+
 const formatTimestamp = (timestamp) => {
   const date = new Date(timestamp);
   const hour = date.getHours();
